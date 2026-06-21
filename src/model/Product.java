503d337d7341d62a00960a7880ab10464a4af396
Girlie-Gumalang <girliegumalang@gmail.com>
2026-06-21 12:17:48 +0800

    refactor: product model properties

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,34 +1,38 @@
 package model;
 
-public class Product extends AbstractItem {
+public class Product {
+    private String id;
+    private String name;
+    private double price;
     private int quantity;
-    private final int LOW_STOCK_THRESHOLD = 5; 
 
     public Product(String id, String name, double price, int quantity) {
-        super(id, name, price);
+        this.id = id;
+        this.name = name;
+        this.price = price;
         this.quantity = quantity;
     }
 
+    // Getters
     public String getId() { return id; }
     public String getName() { return name; }
-    
     public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
-
     public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
 
-
-    public boolean isLowStock() { 
-        return this.quantity <= LOW_STOCK_THRESHOLD; 
+    // Setters
+    public void setId(String id) { 
+        this.id = id; 
     }
-
-    @Override
-    public void displayDetails(int rowNum) {
-        String alert = isLowStock() ? "       [LOW STOCK!]" : "";
-        String stockInfo = quantity + alert; 
-
-        System.out.printf("| %-3d | %-6s | %-20s | PHP %-7.2f | %-20s |\n", 
-                rowNum, id, name, price, stockInfo);
+    
+    public void setName(String name) { 
+        this.name = name; 
+    }
+    
+    public void setPrice(double price) { 
+        this.price = price; 
+    }
+    
+    public void setQuantity(int quantity) { 
+        this.quantity = quantity; 
     }
 }
